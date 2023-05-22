8afe1e19b6dea846ee0f678ba22b44a7aeaa6553
Anushya98 <anushyamary98@gmail.com>
2023-05-22 21:10:58 +0530

    my commit

---
+++ client/src/Dashboard/upcoming-schedule.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import Calendar from 'react-calendar';
+import axios from 'axios';
+import './upcoming.css';
+
+const token = localStorage.getItem("token");
+
+
+const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  
+
+const UpcomingSchedule = () => {
+    const [upcoming, setUpcoming] = useState([])
+    const [value, onChange] = useState(new Date());
+    
+
+    useEffect(() => {
+        axios.get("http://localhost:5000/events", config).then((res => {
+            setUpcoming(res.data.specEvents)
+            console.log("Events data", res.data.specEvents)
+        }));
+    }, []);
+
+
+
+    const handleDateClick = date => {
+        axios
+          .get(`http://localhost:5000/events/${date.toISOString()}`, config)
+          .then(res => {
+        
+          });
+      };
+  
+  //calendar.addEventListener('dateClick', handleDateClick);
+
+  return (
+    <div style={{ marginTop: "3%"}}>
+      <h2>Upcoming Schedules</h2>
+      <Calendar onChange={onChange} value={value} onClickDay={handleDateClick} style={{ width: '100%'}} />
+    </div>
+  );
+}
+
+export default UpcomingSchedule;
